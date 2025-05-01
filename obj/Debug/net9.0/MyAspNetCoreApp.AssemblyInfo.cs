cea5bb2ac8ffb3b70347a50c61b43821a006f0a8
michaelsatumba <mjsatumba@gmail.com>
2025-04-30 17:25:53 -0700

    inital

--- obj/Debug/net9.0/MyAspNetCoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyAspNetCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d32d5a8184205e283ea808b77cb7a64679d855f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
