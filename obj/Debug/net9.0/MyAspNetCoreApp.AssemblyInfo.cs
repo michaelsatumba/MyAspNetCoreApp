efdce42b70856a64173895ac090952d7c1fdb327
michaelsatumba <mjsatumba@gmail.com>
2025-05-01 18:23:48 -0700

    final

--- obj/Debug/net9.0/MyAspNetCoreApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyAspNetCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49aed671702e51b0beb98cf6bbe721f8e2b4a5d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0e2d136e0f97eb45438d74dfeaf58ac6296917")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
